668f2f674b46561e96b9005a5f989dcf1f8ed444
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-25 22:17:01 +0200

    added error state

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -7,17 +7,29 @@ import axios from 'axios'
 function App() {
   const [products, setProducts] = useState<IProduct[]>([])
   const [loading, setLoading] = useState(false)
+  const [error, setError] = useState('')
 
-  useEffect(() => {
+  try {
     async function fetchProducts() {
+      setLoading(true)
       const response = await axios.get<Iproduct[]>('https://fakestoreapi.com/products?limit=8')
       setProducts(response.data)
+      setLoading(false)
     }
+  } catch(e: unknown) {
+    const error = e as AxiosError
+    setLoading(false)
+    setError(error.message)
+  }
+
+  useEffect(() => {
     fetchProducts()
   }, [])
 
   return (
     <div className='container'>
+      {loading && <p>Loading...</p>}
+
       {products.map((product) => (
         <Product key={product.id} product={product} />
       ))}
