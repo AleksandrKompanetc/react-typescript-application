7490ff9187cf356752894ed1fadb02eb17f11640
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-27 23:06:40 +0200

    added changes for component CreateProduct

--- my-app/src/components/CreateProduct.tsx
+++ my-app/src/components/CreateProduct.tsx
@@ -1,10 +1,24 @@
+import React, {useState} from 'react'
+
 export function CreateProduct() {
+  const [value, setValue] = useState('')
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault()
+  }
+
+  const changeHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    setValue(event.target.value)
+  }
+
   return (
-    <form action="">
+    <form onSubmit={submitHandler}>
       <input 
         type="text"
         className="create-input"
         placeholder="Enter product title..."
+        value={value}
+        onChange={changeHandler}
       />
       <button type="submit" className="create-button">Create</button>
     </form>
