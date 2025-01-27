696287d5f932f0308753e232e5d04e0d7d5bcd05
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-27 23:57:14 +0200

    added closing modal window

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -20,7 +20,7 @@ function App() {
         {products.map(product => <Product key={product.id} product={product} />)}
 
         {modal && <Modal title='Create new product'>
-          <CreateProduct />
+          <CreateProduct onCreate={() => setModal(false)} />
         </Modal>}
       </div>
     )
