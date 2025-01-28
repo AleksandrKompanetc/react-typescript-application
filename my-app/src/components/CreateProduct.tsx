ee1e78d93cf9cfe132216bc6836ee8e8fea857f8
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-28 22:18:02 +0200

    added post request

--- my-app/src/components/CreateProduct.tsx
+++ my-app/src/components/CreateProduct.tsx
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { IProduct } from '../models'
+import axios from 'axios'
 
-const submitHandler = (event) => {
-  event.preventDefault()
+const productData: IProduct = {
+  title: 'test product',
+  price: 13.5,
+  description: 'lorem ipsum set',
+  image: 'https://i.pravatar.cc',
+  category: 'electronic',
+  rating: {
+    rate: 42,
+    count: 10
+  }
 }
 
 export function CreateProduct() {
+  const [value, setValue] = useState('')
+
+  const submitHandler = async (event: React.FormEvent) => {
+    event.preventDefault()
+
+    productData.title = value
+
+    const response = await axios.post<IProduct>('https://fakestoreapi.com/products', productData)
+  }
+  
+  const changeHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    setValue(event.target.value)
+  }
+
   return (
     <form onSubmit={submitHandler}>
       <input 
         type="text" 
         className='create-input'
         placeholder='Enter product title...'  
+        value={value}
+        onChange={changeHandler}
       />
 
       <button 
